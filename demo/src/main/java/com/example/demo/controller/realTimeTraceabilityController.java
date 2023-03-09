b73e01901ef40bef3a57713b8abdc9a06f96311b
huangjialong <huangjialong@minotech.cn>
2023-03-09 11:40:37 +0800

    更新

---
+++ demo/src/main/java/com/example/demo/controller/realTimeTraceabilityController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.RealTimeTraceabilityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+
+@RequestMapping("identityGeneration")
+public class realTimeTraceabilityController {
+
+    @Autowired
+    RealTimeTraceabilityService realTimeTraceabilityService;
+
+    /**
+     * 获取所有直接子键key
+     * @return
+     */
+    @GetMapping("/getAllDirectSubChainKey")
+    public List<String> getAllDirectSubChainKey(String mainKey) {
+        return realTimeTraceabilityService.generateKey(mainKey);
+    }
+}
---
+++ demo/src/main/java/com/example/demo/service/impl/RealTimeTraceabilityImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.RealTimeTraceabilityService;
+
+import java.util.List;
+
+public class RealTimeTraceabilityImpl implements RealTimeTraceabilityService {
+
+    @Override
+    public List<String> generateKey(String mainKey) {
+        return null;
+    }
+}
