abf7aabeed30fd37658ae02b1b45ca9c29f0eec3
Gitea <gitea@fake.local>
2023-03-09 14:22:24 +0800

    路由配置修改

--- demo/src/main/java/com/example/demo/controller/identityGenerationController.java
+++ demo/src/main/java/com/example/demo/controller/identityGenerationController.java
@@ -6,12 +6,13 @@ import com.example.demo.service.IdentityGenerationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.security.NoSuchAlgorithmException;
 import java.util.List;
 
-
-@RequestMapping("identityGeneration")
+@RestController
+@RequestMapping("/identityGeneration")
 public class identityGenerationController {
 
     @Autowired
