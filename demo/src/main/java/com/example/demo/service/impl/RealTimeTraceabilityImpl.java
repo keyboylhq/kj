65edbd1e1130a03d07e508715e238182c532babe
Gitea <gitea@fake.local>
2023-03-09 14:23:16 +0800

    服务添加

--- demo/src/main/java/com/example/demo/service/impl/RealTimeTraceabilityImpl.java
+++ demo/src/main/java/com/example/demo/service/impl/RealTimeTraceabilityImpl.java
@@ -1,9 +1,12 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.service.RealTimeTraceabilityService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+
+@Service
 public class RealTimeTraceabilityImpl implements RealTimeTraceabilityService {
 
     @Override
